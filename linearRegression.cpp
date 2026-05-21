f12665cbedf80c835a20721af12f20997d77948e
Dan Truong <dansytruong@gmail.com>
2026-05-21 14:51:30 -0500

    initialize the header and main files

---
+++ linearRegression.cpp
@@ -0,0 +1,5 @@
+#include "linearRegression.h"
+#include <iostream>
+
+using namespace std;
+
---
+++ linearRegression.h
@@ -0,0 +1,9 @@
+#ifndef LINEARREGRESSION_H
+#define LINEARREGRESSION_H
+
+#include<iostream>
+
+using namespace std;
+
+
+#endif // LINEARREGRESSION_H
