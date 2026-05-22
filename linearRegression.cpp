19c7098a4ba35cd99f1f86fb30b45b7ba00a54d2
Dan Truong <dansytruong@gmail.com>
2026-05-22 12:22:59 -0500

    updated the header file

--- linearRegression.cpp
+++ linearRegression.cpp
@@ -1,5 +1,34 @@
 #include "linearRegression.h"
 #include <iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
 
 using namespace std;
 
+LinearRegression::LinearRegression()
+{
+    w = nullptr;
+    b = nullptr;
+    m = nullptr;
+    X = nullptr;
+    Y = nullptr;
+    lr = nullptr;
+    num_iterations = nullptr;
+}
+
+LinearRegression::LinearRegression(double alpha, int maxIterations)
+{
+    lr = new double(alpha);
+    num_iterations = new int(maxIterations);
+}
+
+LinearRegression::~LinearRegression()
+{
+    delete w;
+    delete b;
+    delete m;
+    delete lr;
+    delete num_iterations;
+}
--- linearRegression.h
+++ linearRegression.h
@@ -1,9 +1,71 @@
 #ifndef LINEARREGRESSION_H
 #define LINEARREGRESSION_H
 
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
 
 using namespace std;
 
+class LinearRegression
+{
+    private:
+        //declaring private variables:
+        //weight
+        double* w; 
 
+        //bias
+        double* b;
+
+        //number of features
+        int* m; 
+
+        //samples
+        vector<double>* X;
+        vector<double>* Y;
+
+        //learning rate
+        double* lr;
+
+        //number of iterations
+        int* num_iterations;
+
+    public:
+        //constructor
+        LinearRegression();
+
+        LinearRegression(double alpha, int maxIterations);
+
+        //destructor
+        ~LinearRegression()
+        {
+            delete w;
+            delete b;
+            delete m;
+            delete lr;
+            delete num_iterations;
+        }
+
+        //function to calculate number of features
+        int calculateNumFeatures(vector<double>* X_train);
+
+        //function to calculate the mean squared error
+        double MSE(vector<double>* Y_true, vector<double>* Y_pred);
+
+        //gradient function
+        void gradient(vector<double>* X_train, vector<double>* Y_train, vector<double>* Y_pred);
+
+        //function to update weights and bias
+        void update(vector<double>* X_train, vector<double>* Y_train, vector<double>* Y_pred);
+
+        //fit function
+        void fit(vector<double>* X_train, vector<double>* Y_train, int num_iterations);
+
+        //predict function
+        vector<double> predict(vector<double>* X_test);
+
+
+
+};
 #endif // LINEARREGRESSION_H
